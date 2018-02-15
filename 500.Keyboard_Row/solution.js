5001c897e421572ce2ec176f221d67f9156b1c12
yinwei.sun <18516292795@163.com>
2018-02-15 20:13:47 +0800

    500. Keyboard Row

---
+++ 500.Keyboard_Row/solution.js
@@ -0,0 +1,34 @@
+let isInOneRow = function (map, word) {
+    if (map == null || word == null) {
+        return false;
+    }
+
+    let groupSet = new Set;
+    for (let c of word) {
+        groupSet.add(map.get(c));
+    }
+
+    return groupSet.size === 1;
+};
+
+let findWords = function (words) {
+    const rows = ["qwertyuiopQWERTYUIOP",
+        "asdfghjklASDFGHJKL",
+        "zxcvbnmZXCVBNM"];
+
+    let map = new Map;
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < rows[i].length; j++) {
+            map.set(rows[i].charAt(j), i);
+        }
+    }
+
+    let result = [];
+    for (let word of words) {
+        if (isInOneRow(map, word)) {
+            result.push(word)
+        }
+    }
+
+    return result;
+};
